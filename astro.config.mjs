e192ee4e4e7a1eff34ca30f05bd50e6bbe326541
shekhar5640 <sshekhar5640@gmail.com>
2026-07-25 19:35:01 +0530

    fixed bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://buildyardage.com',
+  build: {
+    inlineStylesheets: 'always',
+  },
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
